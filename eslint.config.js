33897993f329dc45e5528e8d047b135c0e1bb275
Łukasz Brzózko <lukasz9099@gmail.com>
2025-11-15 18:43:15 +0100

    feat: replace Babel with Vite

--- eslint.config.js
+++ eslint.config.js
@@ -8,7 +8,7 @@ export default defineConfig([
   { ...perfectionist.configs["recommended-natural"] },
   {
     files: ["**/*.js"],
-    ignores: ["app.js"],
+    ignores: ["postbuild.js"],
     languageOptions: {
       ecmaVersion: "latest",
       globals: {
@@ -19,7 +19,7 @@ export default defineConfig([
     },
   },
   {
-    files: ["app.js"],
+    files: ["postbuild.js"],
     languageOptions: {
       ecmaVersion: "latest",
       globals: {
--- src/index.user.ts
+++ src/index.user.ts
@@ -1,20 +1,8 @@
-// ==UserScript==
-// @name         Userscript starter
-// @namespace    https://github.com/lukasz-brzozko/userscript-starter
-// @version      2024-05-21
-// @description  Creates a new userscript
-// @author       Łukasz Brzózko
-// @match        https://github.com/
-// @icon         https://github.githubassets.com/favicons/favicon.png
-// @updateURL    https://raw.githubusercontent.com/lukasz-brzozko/userscript-starter/main/dist/index.meta.js
-// @downloadURL  https://raw.githubusercontent.com/lukasz-brzozko/userscript-starter/main/dist/index.user.js
-// @grant        none
-// ==/UserScript==
-
 (function () {
   "use strict";
 
   const IDS = {
+    button: "button",
     container: "container",
   };
 
