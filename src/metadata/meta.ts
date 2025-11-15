f1a1cd1b7adb06a28453b40f0e4f65a75650ed3d
Łukasz Brzózko <lukasz9099@gmail.com>
2025-11-15 19:30:20 +0100

    fix: update @updateURL in meta.ts to point to the correct user.js file

--- src/metadata/meta.ts
+++ src/metadata/meta.ts
@@ -6,7 +6,7 @@
 // @author       Łukasz Brzózko
 // @match        https://github.com/
 // @icon         https://github.githubassets.com/favicons/favicon.png
-// @updateURL    https://raw.githubusercontent.com/lukasz-brzozko/userscript-starter/main/dist/index.meta.js
+// @updateURL    https://raw.githubusercontent.com/lukasz-brzozko/userscript-starter/main/dist/index.user.js
 // @downloadURL  https://raw.githubusercontent.com/lukasz-brzozko/userscript-starter/main/dist/index.user.js
 // @grant        none
 // ==/UserScript==
