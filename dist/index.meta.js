50e9ad042351d42c93f754bcd940041f577a7b8b
GitHub Action <action@github.com>
2025-11-17 12:24:03 +0000

    chore: build dist files [skip ci]

---
+++ dist/index.meta.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         Userscript starter
+// @namespace    https://github.com/lukasz-brzozko/userscript-starter
+// @version      0.0.1
+// @description  Creates a new userscript
+// @author       Łukasz Brzózko
+// @match        https://github.com/
+// @icon         https://github.githubassets.com/favicons/favicon.png
+// @updateURL    https://raw.githubusercontent.com/lukasz-brzozko/userscript-starter/main/dist/index.meta.js
+// @downloadURL  https://raw.githubusercontent.com/lukasz-brzozko/userscript-starter/main/dist/index.user.js
+// @grant        GM_addStyle
+// ==/UserScript==
